24bbb1fbb96475164a7d4c5277780f028db82e6a
Efrain-Alvarez <efrainalvarez7@gmail.com>
2024-11-17 19:32:34 -0800

    Building a dashboard using Dash

--- ship_check_auto.py
+++ ship_check_auto.py
@@ -1,54 +1,15 @@
 ##################################################
 #Automation of Ship Check
-#10/02/24
+#11/17/2024
 #Efrain Alvarez
 ##################################################
 
-from re import findall
-from subprocess import Popen, PIPE
-import os
+import pandas as pd
+from dash import Dash, dcc, html
 
-def ping (host,ping_count):
+app = Dash()
 
-    for ip in host:
-        data = "" #empty string, holds response of ping test
-        output= Popen(f"ping {ip} -n {ping_count}", stdout=PIPE, encoding="utf-8")
+app.layout = [html.Div(children='Hello World')]
 
-        for line in output.stdout:
-            data = data + line #stdout 
-            ping_test = findall("TTL", data) #list of TTL ['TTL', 'TTL', 'TTL', 'TTL']
-        if ping_test:
-            print(f"{ip} : Successful Ping")
-            start_point = data.find("Ping statistics")
-            print(data[start_point:].strip())
-            #output to text file
-            output_file = open("test.txt", "a")
-            output_file.write(data[start_point:].strip())
-            output_file.write('\n\n============================================================\n\n')
-            output_file.close()
-        else:
-            print(f"{ip} : Failed Ping")
-            output_file = open("test.txt", "a")
-            output_file.write(data[start_point:].strip())
-            output_file.write('\n\n============================================================\n\n')
-            output_file.close()
-<<<<<<< HEAD
-=======
-            
-ships = ["www.google.com", "8.8.8.8", "www.youtube.com", "www.facebook.com"]
->>>>>>> 16016022d006dbfaada808441d593a31bcb73b97
-
-
-dir_path = r'D:\efrai\Documents\ReservationApp'
-
-try:
-    file_count = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
-    print(f'File count: {file_count}')
-except FileNotFoundError:
-    print(f"The directory '{dir_path}' does not exist.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-            
-#ships = ["192.168.4.1", "8.8.8.8", "www.google.com", "192.168.4.5"]
-
-#ping(ships,4)
+if __name__ == '__main__':
+    app.run(debug=True)
