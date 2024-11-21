529a09a1426532b822bd10d18f71e375eb0bd469
Efrain-Alvarez <efrainalvarez7@gmail.com>
2024-11-20 23:28:32 -0800

    adding drop down & removing tester file

--- ship_check_auto.py
+++ ship_check_auto.py
@@ -5,11 +5,23 @@
 ##################################################
 
 import pandas as pd
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Output, Input
+import plotly.express as px
 
-app = Dash()
 
-app.layout = [html.Div(children='Hello World')]
+df = pd.read_csv("./customers-100.csv")
+
+app = Dash(__name__)
+
+app.layout = [
+    html.H1(children = "REAP PING", style={'textAlign':'center', 'color': '#4248a9'}),
+    dcc.Dropdown(
+        options = [{'label':name, 'value':name} for name in df["First Name"].unique()],
+        value = df["First Name"].iloc[0]
+    )
+]
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
